a6644abe2f84e6a6f318791202b9cf4ef5626e5f
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2025-02-16 17:21:44 +0500

    Create Task6.c

---
+++ LABS/Lab 4/Task6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+
+void handle_alarm(int signum) {
+    printf("\nAlarm received! Exiting gracefully...\n");
+    exit(0);
+}
+
+int main(){
+    int i = 1;
+    signal(SIGALRM,handle_alarm);
+    alarm(5);
+    
+    while (1){
+        printf("%d second passed...\n",i);
+        i += 1;
+        sleep(1);
+    }
+    
+    return 0;
+}
