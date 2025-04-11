27cd9473878fd8eede97eec5852ff94e2cdac8df
Raghib Rizwan Rabani <142868123+RAGHIBRIZWAN@users.noreply.github.com>
2025-04-11 23:31:17 +0500

    Update main.cpp

--- Project/main.cpp
+++ Project/main.cpp
@@ -3,8 +3,26 @@
 #include <pthread.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>   // For mmap
+#include <sys/types.h>
+#include <fcntl.h>      // For shm_open
+#include <unistd.h>     // For ftruncate
+#include <semaphore.h> 
 
 #define SIZE 50
+#define SHM_NAME "/chat_shm"
+#define SHM_SIZE 1024
+
+typedef struct {
+    char uname[30];
+    char message[256];
+    int chat_active;
+    int sender; // 0: user, 1: librarian
+} SharedChat;
+
+SharedChat *chat;
+sem_t *sem_user_to_lib;
+sem_t *sem_lib_to_user;
 
 typedef struct {
     char name[30];
@@ -234,94 +252,216 @@ void* returnBook(void* arg) {
 }
 
 void* manager(void* arg){
+  pthread_mutex_lock(&lock);
   for (int i = 0; i <= count; i++){
     if(issueArray[i].id == 0){
       continue;
     }
     printf("User Name: %s\nBook Name: %s\nAuthor: %s\nID: %d\n", issueArray[i].uname, issueArray[i].name, issueArray[i].author, issueArray[i].id);
   }
+  pthread_mutex_unlock(&lock);
   return NULL;
+}
+
+
+void* librarianChat(void* arg) {
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed in librarianChat");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
+        perror("ftruncate failed in librarianChat");
+        exit(EXIT_FAILURE);
+    }
+
+    char* buffer = (char *)mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (buffer == MAP_FAILED) {
+        perror("mmap failed in librarianChat");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+      sem_wait(sem_user_to_lib);  // Wait for user message
+
+      if (strlen(buffer) > 0) {
+          if (strcmp(buffer, "exit") == 0) {
+              printf("Chat ended by user.\n");
+              break;
+          }
+          printf("[User]: %s\n", buffer);
+      }
+
+      printf("[Librarian]: ");
+      fgets(buffer, SHM_SIZE, stdin);
+      buffer[strcspn(buffer, "\n")] = '\0';
+
+      sem_post(sem_lib_to_user);  // Notify user of reply
+
+      if (strcmp(buffer, "exit") == 0) {
+          break;
+      }
+    } 
+
+    munmap(buffer, SHM_SIZE);
+    close(shm_fd);
+    return NULL;
+}
+
+void* userChat(void* arg) {
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("shm_open failed in userChat");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
+        perror("ftruncate failed in userChat");
+        exit(EXIT_FAILURE);
+    }
+
+    char* buffer = (char *)mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (buffer == MAP_FAILED) {
+        perror("mmap failed in userChat");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+      printf("[User]: ");
+      fgets(buffer, SHM_SIZE, stdin);
+      buffer[strcspn(buffer, "\n")] = '\0';
+
+      sem_post(sem_user_to_lib);  // Notify librarian
+
+      if (strcmp(buffer, "exit") == 0) break;
+
+      sem_wait(sem_lib_to_user);  // Wait for librarian reply
+
+      if (strlen(buffer) > 0) {
+          printf("[Librarian]: %s\n", buffer);
+          buffer[0] = '\0';
+      }
+    }
+
+    munmap(buffer, SHM_SIZE);
+    close(shm_fd);
+    return NULL;
 }
 
 int main() {
     pthread_t threads[10];
-    int code;
-    int choice;
-    int check = true;
+    int code, choice;
+    int check = 1;
 
-    printf("Enter any one:\n 1) Library\n 2) User\n 3) Exit\n");
-    scanf("%d", &choice);
+    sem_user_to_lib = sem_open("/sem_user_to_lib", O_CREAT, 0666, 0);
+    sem_lib_to_user = sem_open("/sem_lib_to_user", O_CREAT, 0666, 0);
+
+
+    if (sem_user_to_lib == SEM_FAILED || sem_lib_to_user == SEM_FAILED) {
+
+        perror("sem_open failed");
+
+        exit(EXIT_FAILURE);
+
+    }
 
     bookData data;
     issueData issue;
-    
     void* authResult;
-    
-    while(check){
-      printf("Enter any one:\n 1) Library\n 2) User\n 3) Exit\n");
-      scanf("%d", &choice);
-      switch(choice){
-        case 1:
-          printf("Enter the code to access library: ");
-          scanf("%d", &code);
-          pthread_create(&threads[0], NULL, authentication, &code);
-          pthread_join(threads[0], &authResult);
 
-          if (*(int*)authResult == 1) {
-            int choice2;
-            printf("Enter any one:\n 1) Add Book\n 2) Delete Book\n 3) Update Book\n 4) Search Book\n 5) Track Users\n");
-            scanf("%d",&choice2);
-            switch(choice2){
-                case 1:
-                    pthread_create(&threads[1], NULL, addBook, &data);
-                    pthread_join(threads[1], NULL);
-                    break;
-                case 2:
-                    pthread_create(&threads[2], NULL, deleteBook, NULL);
-                    pthread_join(threads[2], NULL);
-                    break;
-                case 3:
-                    pthread_create(&threads[3], NULL, updateBook, &data);
-                    pthread_join(threads[3], NULL);
-                    break;
-                case 4:
-                    pthread_create(&threads[4], NULL, searchBook, NULL);
-                    pthread_join(threads[4], NULL);
-                    break;
-                case 5:
-                    pthread_create(&threads[5], NULL, manager, NULL);
-                    pthread_join(threads[5], NULL);
-                    break;
-                default:
-                    printf("Wrong Input!\n");
-                    break;
+    while (check) {
+        printf("\nEnter any one:\n 1) Library\n 2) User\n 3) Exit\nChoice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the code to access library: ");
+                scanf("%d", &code);
+
+                pthread_create(&threads[0], NULL, authentication, &code);
+                pthread_join(threads[0], &authResult);
+
+                if (*(int*)authResult == 1) {
+                    int choice2;
+                    printf("\nEnter any one:\n 1) Add Book\n 2) Delete Book\n 3) Update Book\n 4) Search Book\n 5) Track Users\n 6) Chat\nChoice: ");
+                    scanf("%d", &choice2);
+
+                    switch (choice2) {
+                        case 1:
+                            pthread_create(&threads[1], NULL, addBook, &data);
+                            pthread_join(threads[1], NULL);
+                            break;
+                        case 2:
+                            pthread_create(&threads[2], NULL, deleteBook, NULL);
+                            pthread_join(threads[2], NULL);
+                            break;
+                        case 3:
+                            pthread_create(&threads[3], NULL, updateBook, &data);
+                            pthread_join(threads[3], NULL);
+                            break;
+                        case 4:
+                            pthread_create(&threads[4], NULL, searchBook, NULL);
+                            pthread_join(threads[4], NULL);
+                            break;
+                        case 5:
+                            pthread_create(&threads[5], NULL, manager, NULL);
+                            pthread_join(threads[5], NULL);
+                            break;
+                        case 6:
+                            pthread_create(&threads[8], NULL, librarianChat, NULL);
+                            pthread_join(threads[8], NULL);
+                            break;
+                        default:
+                            printf("Wrong Input!\n");
+                            break;
+                    }
+                } else {
+                    printf("Wrong Password!\n");
+                }
+                break;
+
+            case 2: {
+                int choice3;
+                printf("\nEnter any one:\n 1) Issue Book\n 2) Return Book\n 3) Chat\nChoice: ");
+                scanf("%d", &choice3);
+
+                switch (choice3) {
+                    case 1:
+                        pthread_create(&threads[6], NULL, issueBook, &issue);
+                        pthread_join(threads[6], NULL);
+                        break;
+                    case 2:
+                        pthread_create(&threads[7], NULL, returnBook, NULL);
+                        pthread_join(threads[7], NULL);
+                        break;
+                    case 3:
+                        pthread_create(&threads[9], NULL, userChat, NULL);
+                        pthread_join(threads[9], NULL);
+                        break;
+                    default:
+                        printf("Wrong Input!\n");
+                        break;
+                }
+                break;
             }
-        } else {
-            printf("Wrong Password!\n");
+
+            case 3:
+                check = 0;
+                break;
+
+            default:
+                printf("Wrong Input!\n");
+                break;
         }
-        break;
-      case 2:
-        int choice3;
-        printf("Enter any one:\n 1) Issue Book\n 2) Return Book\n");
-        scanf("%d",&choice3);
-        switch(choice3){
-          case 1:
-            pthread_create(&threads[6], NULL, issueBook, (void *)&issue);
-            pthread_join(threads[6], NULL);
-            break;
-          case 2:
-            pthread_create(&threads[7], NULL, returnBook, NULL);
-            pthread_join(threads[7], NULL);
-            break;
-          }
-          break;
-      case 3:
-        check = false;
-        break;
-      default:
-        printf("Wrong Input!");
-        break;
     }
-  }
+    free(authResult);
+
+    sem_close(sem_user_to_lib);
+    sem_close(sem_lib_to_user);
+    sem_unlink("/sem_user_to_lib");
+    sem_unlink("/sem_lib_to_user");
+    shm_unlink(SHM_NAME);
+
     return 0;
 }
